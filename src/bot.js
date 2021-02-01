d8714af6c5100e1a7fa95290ddd5526e986e3a93
aomine <aomine1745@gmail.com>
2021-02-01 17:29:45 -0500

    fix heroku conflict

--- src/bot.js
+++ src/bot.js
@@ -22,7 +22,7 @@ client.on('message', message => {
   if (!message.content.startsWith(prefix) || message.author.bot) return
 
   const args = message.content.slice(prefix.length).trim().split(/ +/)
-  const commandName = args.shift()?.toLowerCase()
+  const commandName = args.shift().toLowerCase()
 
   const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName))
 
